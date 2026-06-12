b591105f13f9c3c7b7fc15ae6c42a078ba92881c
Lora Bell <plutovka-z@rambler.ru>
2026-06-12 18:11:54 +0300

    add search by name

--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -6,6 +6,7 @@ import { ImageCard } from "./ImageCard";
 export const Character = () => {
 
     const [persons, setPersons] = useState(Array<IPerson>)
+    const [searchPerson, setSearchPerson] = useState<string>("")
 
     useEffect(() => {
         // const load = async() => {
@@ -18,34 +19,62 @@ export const Character = () => {
 
         fetch("https://potterapi-fedeperin.vercel.app/en/characters")
             .then((res) => {
+                if (!res.ok) {
+                    throw new Error()
+                }
                 return res.json()
             })
             .then((data) => {
                 setPersons(data)
             })
+            .catch((e) => setPersons([]))
     }, [])
 
+    const filterPerson = persons.filter(person => {
+        return person.nickname.toLowerCase().includes(searchPerson.toLowerCase()) ||
+            person.fullName.toLowerCase().includes(searchPerson.toLowerCase())
+
+    })
 
     return (
         <>
             {persons.length > 0 ?
-                <div className={styles.cards}>
-                    {persons.map((person) => {
-                        return (
-                            <div key={person.index} className={styles.personCard}>
-                                <ImageCard src={person.image} />
-                                <div className={styles.infoPerson}>
-                                    <h3 className={styles.nickname}>                                        
-                                        {person.nickname}     
-                                        <span className={styles.fullName}>{person.fullName}</span>                                   
-                                    </h3>                                    
-                                    <p>birthdate: {person.birthdate}</p>
+                <div className={styles.container}>
+                    <input
+                        type="text"
+                        placeholder="🔍 Поиск по имени..."
+                        className={styles.inputSearch}
+                        value={searchPerson}
+                        onChange={(elem) => setSearchPerson(elem.target.value)}
+                    >
+                    </input>
+                    <div className={styles.cards}>
+                        {persons.map((person) => {
+                            const isVisible =
+                                person.nickname.toLowerCase().includes(searchPerson.toLowerCase()) ||
+                                person.fullName.toLowerCase().includes(searchPerson.toLowerCase())
+
+                            return (
+                                <div
+                                    key={person.index}
+                                    className={styles.personCard}
+                                    style={{ display: isVisible ? "flex" : "none" }}
+                                >
+                                    <ImageCard src={person.image} />
+                                    <div className={styles.infoPerson}>
+                                        <h3 className={styles.nickname}>
+                                            {person.nickname}
+                                            <span className={styles.fullName}>{person.fullName}</span>
+                                        </h3>
+                                        <p>birthdate: {person.birthdate}</p>
+                                    </div>
                                 </div>
-                            </div>
-                        )
-                    })}
+                            )
+                        })}
+                    </div>
+                    {filterPerson.length === 0 && <h3 className={styles.info}>Персонажи с таким именем не найдены</h3>}
                 </div>
-                : <h1>The request failed</h1>
+                : <h3 className={styles.info}>Персонажи не получены<br />Попробуйте зайти на страницу позже</h3>
             }
 
         </>
