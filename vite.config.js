f3120b8b293ca1ae378ed0ba59aada4fe436c1af
Lora Bell <plutovka-z@rambler.ru>
2026-06-14 21:46:44 +0300

    fix vite build for github

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
     base: '/projectRAM/',
-    plugins: [react()]
+    plugins: [react()],
+    build: {
+        outDir: 'docs',
+    },
 });
